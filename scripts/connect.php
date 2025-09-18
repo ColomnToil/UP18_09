d15db702959d93c5dd98b2c76b2ec96be831ff74
ColomnToil <colomntoil12@mail.ru>
2025-09-18 16:45:51 +0300

    Добавили подключение к базе данных

---
+++ scripts/connect.php
@@ -0,0 +1,13 @@
+<?
+require_once('app-config.php');
+$mysqli = new mysqli(
+    DATABASE_HOST,
+    USERNAME,
+    PASSWORD,
+    DATABASE_NAME
+);
+if ($mysqli->connect_errno) {
+    echo 'Не удалось подключиться к MySQL: ' . $mysqli->connect_error;
+} else {
+    echo 'Подключение к MySQL выполнено успешно';
+}
