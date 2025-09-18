388f414e5a0716bfde283d2ba6e30cb86b5e8cb8
ColomnToil <colomntoil12@mail.ru>
2025-09-18 16:51:24 +0300

    Добавили занесение ифнормации в базу данных

---
+++ scripts/create-user.php
@@ -0,0 +1,57 @@
+<?php
+require_once("connect.php");
+
+$first_name = trim($_REQUEST['first_name']);
+$last_name = trim($_REQUEST['last_name']);
+$email = trim($_REQUEST['email']);
+$url_site = trim($_REQUEST['url_site']);
+$vk = trim($_REQUEST['vk']);
+
+$insert_sql = <<<HEREDOC
+INSERT INTO `users` (`first_name`, `last_name`, `email`,
+`url_site`, `vk`) VALUES (
+'$first_name',
+'$last_name',
+'$email',
+'$url_site',
+'$vk'
+)
+HEREDOC;
+// выполняем запрос вставки данных о пользователе
+$mysqli->query($insert_sql) or die('Ошибка вставки данных: ' .
+    $mysqli->errno . '. ' . $mysqli->error);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="../css/phpMM.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+    <div class="wrap">
+        <div id="header">
+            <h1>PHP & MySQL: The Missing Manual</h1>
+        </div>
+        <div id="example">Регистрация</div>
+        <?php require("menu.php"); ?>
+        <div id="content">
+            <p>Это запись той информации, которую вы отправили:</p>
+            <p>
+                Имя: <?php echo $first_name . " " . $last_name ?><br />
+                Адрес электронной почты: <?php echo $email ?><br />
+                <a href="//<?php echo $url_site; ?>" target="_blank">Ваш персональный
+                    сайт</a><br />
+                <a href="https://vk.com/<?php echo $vk; ?>" target="_blank">Ваша
+                    страница ВКонтакте<br />
+            </p>
+        </div>
+        <div id="footer"></div>
+    </div>
+</body>
+
+</html>
